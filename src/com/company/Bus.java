1d83d2741aad7642f6d4bada827ea6e1125f9a17
Csilla <posa.csilla@gmail.com>
2020-10-18 10:41:38 +0200

    Arrays, instance variables, constructor, methods of Bus class
    Created the necessary parts of the class, commented what each part is supposed to do

--- src/com/company/Bus.java
+++ src/com/company/Bus.java
@@ -10,4 +10,34 @@ public class Bus {
     //instance variables for place of departure and destination:
     private String placeOfDeparture;
     private String destination;
+
+    //constructor for the instance variables and arrays
+    public Bus(Passenger[][] leftSide, Passenger[][] rightSide, String placeOfDeparture, String destination) {
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
+        this.placeOfDeparture = placeOfDeparture;
+        this.destination = destination;
+    }
+
+    //expected functions:
+
+    //check if there is available seat on the bus
+    public boolean checkIfThereIsAvailableSeat() {
+
+    }
+
+    //reserve the next available seat by placing Passenger p in one of the two arrays
+    public void reserveNextAvailableSeat(Passenger p) {
+
+    }
+
+    //delete seat reservation based on the place on the bus
+    public void deleteSeatReservation(String side, int row, int seat) {
+
+    }
+
+    //check if Passenger is on the bus, if yes, delete
+    public void deletePassengerReservation(Passenger p) {
+        
+    }
 }
