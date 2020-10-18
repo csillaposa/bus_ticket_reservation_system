a0f5bb14e3c87213033afeead73cabe489b0b9af
Csilla <posa.csilla@gmail.com>
2020-10-18 10:34:51 +0200

    Creating the classes and starting with Bus class

--- src/com/company/Bus.java
+++ src/com/company/Bus.java
@@ -4,4 +4,10 @@ public class Bus {
 
     //two 2x20 two-dimensional arrays that hold Passenger objects
     //represent the left and right side of the bus
+    Passenger[][] leftSide = new Passenger[2][20];
+    Passenger[][] rightSide = new Passenger[2][20];
+
+    //instance variables for place of departure and destination:
+    private String placeOfDeparture;
+    private String destination;
 }
