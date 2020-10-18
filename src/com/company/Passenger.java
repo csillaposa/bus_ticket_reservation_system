48d2ca4059b727633a1701f760c26ba7539cc4e7
Csilla <posa.csilla@gmail.com>
2020-10-18 10:58:40 +0200

    Set up the Passenger class
    Created the array list, fields, constructor, getters, and the necessary methods

--- src/com/company/Passenger.java
+++ src/com/company/Passenger.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Passenger {
+
+    //the array list that holds Ticket objects
+    private ArrayList<Ticket> tickets = new ArrayList<Ticket>();
+
+    //instance variables for this class:
+    private String name;
+    private String dob;
+    private String address;
+
+    //constructor
+    public Passenger(ArrayList<Ticket> ticketArray, String name, String dob, String address) {
+        this.tickets = ticketArray;
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+    }
+
+    //expected functions:
+
+    //getters:
+    public ArrayList<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    //create a new ticket and add it to the array list
+    public void createTicket(String date, String departure, String destination) {
+
+    }
+
+    //override the toString function and represent the Passenger object with nicely formatted info about him/her instead
+    @Override
+    public String toString() {
+        
+    }
 }
