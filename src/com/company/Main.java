2d010d977800b08d9b69832aef5ae738a1a529ee
Csilla <posa.csilla@gmail.com>
2020-10-21 22:43:18 +0200

    First test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Bus bus = new Bus("Győr", "Komárom");
+	    Passenger passenger = new Passenger("Adam", "1993.", "Komarom");
+	    Ticket ticket = new Ticket("Adam", "2020.", "Gyor", "Komarom");
+
+
+		System.out.println(bus.checkIfThereIsAvailableSeat());
+		bus.reserveNextAvailableSeat(passenger);
+		System.out.println(bus.toString());
+
     }
 }
