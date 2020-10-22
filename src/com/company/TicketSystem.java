f61d2edb477b7987ee731c0e505bd8fea8e6eece
Csilla <posa.csilla@gmail.com>
2020-10-22 18:48:35 +0200

    TicketSystem array lists and functions
    Created the two array lists for Bus and Passanger objects, created the functions which will add the objects to the array lists

--- src/com/company/TicketSystem.java
+++ src/com/company/TicketSystem.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class TicketSystem {
+    private ArrayList<Bus> bus = new ArrayList<Bus>();
+    private ArrayList<Passenger> passenger = new ArrayList<Passenger>();
+
+    //functions
+
+    //creates a new Bus object and add it to the ArrayList
+    public void createBus(String departure, String destination) {
+        bus.add(new Bus(departure, destination));
+    }
+
+    //creates a new Passenger object and add it to the ArrayList
+    public void createPassenger(String name, String dob, String address) {
+        passenger.add(new Passenger(name, dob, address));
+    }
+
+    //
 }
